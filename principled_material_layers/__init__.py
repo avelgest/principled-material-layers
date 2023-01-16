2ab36c9ce90c7256a7ecadc3d8d9f181fa80d2a9
Avelgest <avelgest@outlook.com>
2023-01-16 18:25:30 +0000

    Version 0.6.2

--- principled_material_layers/__init__.py
+++ principled_material_layers/__init__.py
@@ -4,7 +4,7 @@ bl_info = {
     "name": "Principled Material Layers",
     "description": "Addon for painting node-based material layers",
     "author": "Avelgest",
-    "version": (0, 6, 0),
+    "version": (0, 6, 2),
     "blender": (3, 0, 1),
     "category": "Material",
     "location": ("View3D > Texture Paint Sidebar > Material Layers "
